9f2c0caced0e8e2ecf9ecefe2ac113a8d8194286
claudiahW <murungiclaud@gmail.com>
2023-09-06 20:16:35 +0300

    complete js

--- script.js
+++ script.js
@@ -18,11 +18,26 @@ function addItem(){
     divParent.className = "item";
     divParent.innerHTML = '<div>'+input.value+'</div>';
 
-   //create our two icons
+   //append our two icons
    checkIcon.className = "fas fa-check-square";
    checkIcon.style.color = "lightgray";
    checkIcon.addEventListener("click", function(){
     checkIcon.style.color = "limegreen";
    })
+   divChild.appendChild(checkIcon);
+
+   trashIcon.className = "fas fa-trash";
+   trashIcon.style.color = "darkgray";
+   trashIcon.addEventListener("click", function(){
+    divParent.remove();
+   })
+ divChild.appendChild(trashIcon);
+
+ divParent.appendChild(divChild);
+
+ toDoItems.appendChild(divParent);
+
+ input.value = "";
 
 }
+
