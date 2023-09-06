766c843a70942aa6f6d22a99557d0020de347022
claudiahW <murungiclaud@gmail.com>
2023-09-06 19:44:23 +0300

    declare variables in the additem function

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ input.addEventListener("keydown",function(event){
     if(event.key === "Enter")
     addItem();
 })
+//create addItem function
+function addItem(){
+    let divParent = document.createElement("div");
+    let divChild = document.createElement("div");
+    let checkIcon =document.createElement("i");
+    let trashIcon = document.createElement("i");
+
+}
